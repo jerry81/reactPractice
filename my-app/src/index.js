64f6567ea7819baea587794c1401843761e05362
Raskolnikov <jerry.tann@opteyes.cn>
2021-03-23 17:03:04 +0800

    feat: add calculate winner code (doesnt work)

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -44,7 +44,13 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = `Next player: ${this.state.xIsNext ? "X" : "O"}`;
+      const winner = calculateWinner(this.state.squares)
+    let status
+    if (winner) {
+        status = `Winnder ${winner}`
+    } else {
+        status = `Next player: ${this.state.xIsNext ? "X" : "O"}`;
+    }
 
     return (
       <div>
@@ -83,6 +89,26 @@ class Game extends React.Component {
       </div>
     );
   }
+}
+
+function calculateWinner(squares) {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+      ];
+      lines.forEach((line) => {
+          const [a,b,c] = line
+          if (squares[a] === squares[b] && squares[b] === squares[c]) {
+              return squares[a]
+          }
+      })
+      return null
 }
 
 // ========================================
